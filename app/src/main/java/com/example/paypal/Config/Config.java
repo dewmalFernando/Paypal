34a9a9d031f92217a8db7c9a419d6caba0f1a252
dewmalFernando <dewmalfernando@gmail.com>
2019-09-14 12:56:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/paypal/Config/Config.java
@@ -0,0 +1,5 @@
+package com.example.paypal.Config;
+
+public class Config {
+    public static final String PAYPAL_CLIENT_ID = "AdImZVhRjuaEx-PtG6KH9BXyyuDK_VqrncuyEPmwmX8LzuVr8nj8dLWH-oNUV8xD830tZO1I_f1iDK0n";
+}
